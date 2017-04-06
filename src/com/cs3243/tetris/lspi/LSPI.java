5fcb0a4f1540bfb0531858d8888fb63bd35e6142
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-04-06 23:22:31 +0800

    update more

--- src/com/cs3243/tetris/lspi/LSPI.java
+++ src/com/cs3243/tetris/lspi/LSPI.java
@@ -21,16 +21,18 @@ public class LSPI {
 			Sample sample = samples[i];
 			int[] policyAction = policy.getAction(sample.state);
 
-			// TODO
 			for (int j = 0; j < policy.features.length; j++) {
 				Feature feature = policy.features[j];
 
+				NextState nsSample = sample.nextState;
+				
+				// TODO: this should be the score of feature acting on nsSample
 				samplesArray[i][j] = feature.getScore();
 				
-				NextState ns = new NextState();
-				ns.generateNextState(sample.state, policyAction);
+				NextState nsPolicy = new NextState();
+				nsPolicy.generateNextState(sample.state, policyAction);
 				
-				feature.updateScore(ns, 0, 0);
+				// TODO: this should be the score of feature acting on nsPolicy
 				policyArray[i][j] = feature.getScore();
 			}
 			
@@ -47,7 +49,7 @@ public class LSPI {
 		Matrix weightsMatrix = LHSMatrix.solve(RHSMatrix);
 		
 		for (int i = 0; i < policy.features.length; i++) {
-			policy.weights[i] = weightsMatrix.get(i, 0);
+			policy.features[i].setFeatureWeight(weightsMatrix.get(i, 0));
 		}
 	}
 }
--- src/com/cs3243/tetris/lspi/Policy.java
+++ src/com/cs3243/tetris/lspi/Policy.java
@@ -1,19 +1,26 @@
 package com.cs3243.tetris.lspi;
 
+import com.cs3243.tetris.NextState;
+import com.cs3243.tetris.PlayerSkeleton;
 import com.cs3243.tetris.State;
 import com.cs3243.tetris.features.Feature;
+import com.cs3243.tetris.heuristics.Heuristic;
 
 public class Policy {
 	Feature[] features;
-	double[] weights;
-	
+
 	Policy(Feature[] fs) {
 		features = fs;
-		weights = new double[features.length];
 	}
-	
-	// TODO
+
 	public int[] getAction(State state) {
-		return new int[0];
+		int[][] legalMoves = state.legalMoves();
+		Heuristic heuristic = new Heuristic();
+		heuristic.setFeatures(features);
+
+		int legalIndex = (new PlayerSkeleton())
+				.pickMove(state, legalMoves, new NextState(), heuristic);
+		
+		return legalMoves[legalIndex];
 	}
 }
